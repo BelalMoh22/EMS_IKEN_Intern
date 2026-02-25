8638413b03028f39366b8ea5b4408896989c1b9c
Belal Mohamed <ibelalmohammed@gmail.com>
2026-02-25 03:56:11 +0200

    Make JwtAuthentaction and UnAuthorized If there is no Token

---
+++ EmployeeService/Features/Auth/Login/LoginHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeService.Features.Auth.Login
+{
+    public class LoginHandler : IRequestHandler<LoginCommand, string>
+    {
+        private readonly UserRepository _userRepository;
+        private readonly IJwtTokenGenerator _jwt;
+
+        public LoginHandler(UserRepository userRepository,IJwtTokenGenerator jwt)
+        {
+            _userRepository = userRepository;
+            _jwt = jwt;
+        }
+
+        public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByUsernameAsync(request.UserName);
+
+            if (user is null)
+                throw new UnauthorizedAccessException("Invalid credentials.");
+
+            var isValid = BCrypt.Net.BCrypt.Verify(request.Password,user.PasswordHash);
+
+            if (!isValid)
+                throw new UnauthorizedAccessException("Invalid credentials.");
+
+            return _jwt.GenerateToken(user);
+        }
+    }
+}
---
+++ EmployeeService/Infrastructure/Security/JWT Generator.cs
@@ -0,0 +1,35 @@
+﻿namespace EmployeeService.Infrastructure.Security
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.UniqueName, user.Username),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
